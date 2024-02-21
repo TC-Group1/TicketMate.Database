adcb9b59ce20b83778287c2f584d74498363be46
Erik Hedman <erikdavid.hedman21@outlook.com>
2024-02-20 22:02:50 -0500

    Dropped the Username column and updated each column with data in it

--- ticketmate-docker/Mysql/data/c.ticketmate_refactorUsersTable.sql
+++ ticketmate-docker/Mysql/data/c.ticketmate_refactorUsersTable.sql
@@ -23,3 +23,15 @@ Add Column Avatar nvarchar(256) Null;
 
 Alter Table Users
 Add Column IsActive TinyInt Not Null Default(1);
+
+Alter Table Users
+Drop Column Username;
+
+Update Users
+Set PhoneNumber = 9998887777, Email = "john.man@theemail.com", FirstName = "John", LastName = "Man", Avatar = "customAvatar"
+Where Id = 1;
+
+Update Users
+Set PhoneNumber = 8889995555, Email = "mary.woman@theemail.com", FirstName = "Mary", LastName = "Woman", Avatar = "customFemaleAvatar"
+Where Id = 2;
+
